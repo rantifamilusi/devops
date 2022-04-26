00af5979f61da508d09ad09d2c2023b82358eca3
Ranti <babarantifamilusi@IBM.local>
2022-04-25 21:14:47 -0600

    update k8s config

--- SampleApp/SampleApp.Api/Startup.cs
+++ SampleApp/SampleApp.Api/Startup.cs
@@ -36,7 +36,7 @@ namespace SampleApp.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
