5f6d901306d29511e7bcb9f6183e447e790b88ae
rantifamilusi <familusi.babaranti@gmail.com>
2022-04-28 18:10:55 -0600

    add cors

--- SampleApp/SampleApp.Api/Startup.cs
+++ SampleApp/SampleApp.Api/Startup.cs
@@ -25,12 +25,13 @@ namespace SampleApp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+        
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SampleApp.Api", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,11 @@ namespace SampleApp.Api
             }
 
             app.UseFileServer();
+            app.UseCors(x => x
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed(origin => true)
+                    .AllowCredentials());
 
             app.UseRouting();
 
